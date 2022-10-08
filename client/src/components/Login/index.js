1b53efdfb34d66039bdd94d93c714292a4680e45
Derrick James <ballyouth@gmail.com>
2022-10-08 11:12:11 -0400

    login/signup script added

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,18 +3,20 @@ import React from "react";
 function Login() {
 
     return (
+        <>
         <form>
             <label>
                 Username:
-            <input type="text" name="username" />
+                <input type="text" name="username" />
             </label>
             <label>
                 Password:
-            <input type="text" name="password" />
+                <input type="text" name="password" />
             </label>
             <input type="submit" value="Submit" />
-        
         </form>
+        <script src="../assets/js/login.js"></script>
+        </>
     )
 }
 
