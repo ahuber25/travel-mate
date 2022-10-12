ba24b19262f8db3f06fa707f82c78893dff34f23
Derrick James <ballyouth@gmail.com>
2022-10-11 20:00:17 -0400

    test2

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,11 +1,37 @@
 import React from "react";
+import {useEffect} from "react"
 
 function Home() {
-        return (
+    const myFunction = () => {
+        // your logic here
+        console.log('pressed Enter ✅');
+      };
+    
+      useEffect(() => {
+        const keyDownHandler = event => {
+          console.log('User pressed: ', event.key);
+    
+          if (event.key === 'Enter'|| event.key === 'return') {
+            event.preventDefault();
+    
+            // 👇️ your logic here
+            myFunction();
+          }
+        };
+    
+        document.addEventListener('keydown', keyDownHandler);
+    
+        return () => {
+          document.removeEventListener('keydown', keyDownHandler);
+        };
+      }, []);
+    
+    return (
         <section className="home">
             <div className="section-titles">
                 <h3>Where shall we take you?</h3>
-                <p><input type="text" placeholder="Search.."></input></p>
+                <p><input id="city" type="text" placeholder="Search.." 
+                ></input></p>
             </div>
             <div className="info">
                 <p>Travel Mate description here</p>
